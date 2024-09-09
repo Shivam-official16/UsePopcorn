542cac8f4c4eb660d19acbf7ce8caa2bde45c377
Shivam Singh <shivamsingh31116@gmail.com>
2024-09-09 22:19:37 +0530

    version 2

---
+++ src/useMovies.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(
+    function () {
+      callback?.();
+      const controller = new AbortController();
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const result = await fetch(
+            `http://www.omdbapi.com/?apikey=5c4b19cb&s=${query}`,
+            { signal: controller.signal }
+          );
+          if (!result.ok)
+            throw new Error("Something went wrong with fetching movies");
+          const data = await result.json();
+          if (data.Response === "False") throw new Error("Movie not found ");
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          console.error(err.message);
+          if (err.name !== "AbortError") setError(err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      //   handlesCLoseMovie();
+      fetchMovies();
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+  return { movies, isLoading, error };
+}
